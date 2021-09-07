397e2f0460db377c86b2b71cb4b3cebc5c77e133
Leonardo Lopes <leonardolopesb97@hotmail.com>
2021-09-07 01:31:10 -0300

    Adicionando firebaseao projeto

---
+++ src/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+  apiKey: "AIzaSyCVgb8Hr6bhLxD3JcOUKPsQgoC7M1PSUo0",
+  authDomain: "whats-clone-leoolopes01.firebaseapp.com",
+  projectId: "whats-clone-leoolopes01",
+  storageBucket: "whats-clone-leoolopes01.appspot.com",
+  messagingSenderId: "935584510385",
+  appId: "1:935584510385:web:6ffe8f809abbb353168d90",
+};
